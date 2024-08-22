e25e8b7b070380f43f25f8e2368bed6a2f14db1f
etorkay <50207979+etorkay@users.noreply.github.com>
2024-08-22 16:14:35 +0000

    All traces removed

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -41,7 +41,7 @@ describe('renders the app', () => {
   });
 
   it('should render the title', async () => {
-    expect(document.title).toBe("Michael D'Angelo");
+    expect(document.title).toBe("Nero NOVOR");
   });
 
   it('can navigate to /about', async () => {
