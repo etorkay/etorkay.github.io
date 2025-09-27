513f0532755a48e3fe2f3354cc8f9117cfedf2b9
etorkay <50207979+etorkay@users.noreply.github.com>
2025-09-27 05:20:59 +0000

    lint config added fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,22 @@
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
-import json from "@eslint/json";
-import markdown from "@eslint/markdown";
-import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  pluginReact.configs.flat.recommended,
-  { files: ["**/*.json"], plugins: { json }, language: "json/json" },
-  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc" },
-  { files: ["**/*.json5"], plugins: { json }, language: "json/json5" },
-  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/commonmark" },
-  { files: ["**/*.css"], plugins: { css }, language: "css/css" },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+    plugins: { react: pluginReact },
+    rules: {
+      "react/react-in-jsx-scope": "off" // not needed for React 17+
+    }
+  }
 ]);
+
