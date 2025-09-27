23f7110d32f471857944d240e3d6f48fa0b855e5
etorkay <50207979+etorkay@users.noreply.github.com>
2025-09-27 05:25:33 +0000

    lint config added fixed again

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,6 @@
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      globals: {
-        ...globals.browser,
-        ...globals.node
-      }
-    },
-    plugins: { react: pluginReact },
-    rules: {
-      "react/react-in-jsx-scope": "off" // not needed for React 17+
-    }
-  }
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
 ]);
-
