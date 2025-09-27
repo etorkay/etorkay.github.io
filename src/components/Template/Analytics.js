f38f9ceb078ab61e2b8fc751ac662e9ecf79d212
etorkay <50207979+etorkay@users.noreply.github.com>
2025-09-27 17:42:54 +0000

    analytics provisioned for failure

--- src/components/Template/Analytics.js
+++ src/components/Template/Analytics.js
@@ -1,22 +1,22 @@
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import ReactGA from 'react-ga4';
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import ReactGA from "react-ga4";
 
 const { NODE_ENV, REACT_APP_GA_TRACKING_ID } = process.env;
 
-if (NODE_ENV === 'production') {
+if (NODE_ENV === "production" && REACT_APP_GA_TRACKING_ID) {
   ReactGA.initialize(REACT_APP_GA_TRACKING_ID);
+} else {
+  console.warn("⚠️ Skipping Google Analytics: GA_TRACKING_ID not set");
 }
 
 const Analytics = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    if (NODE_ENV === 'production') {
-      ReactGA.set({
-        page: pathname,
-      });
-      //ReactGA.pageview(pathname);
+    if (NODE_ENV === "production" && REACT_APP_GA_TRACKING_ID) {
+      ReactGA.set({ page: pathname });
+      // ReactGA.pageview(pathname);
     }
   }, [pathname]);
 
