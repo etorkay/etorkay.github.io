f1402b924dc67f6e5f1e103ee084bdbadd6f8df1
etorkay <50207979+etorkay@users.noreply.github.com>
2025-09-28 22:57:11 +0000

    remove esling.config.mjs

--- eslint.config.mjs
+++
@@ -1,6 +0,0 @@
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-]);
