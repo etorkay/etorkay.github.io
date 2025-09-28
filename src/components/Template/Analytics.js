aa5c531696958a495d346d8e4c8766ac87f54701
etorkay <50207979+etorkay@users.noreply.github.com>
2025-09-28 17:12:24 +0000

    variable replaced

--- src/components/Template/Analytics.js
+++ src/components/Template/Analytics.js
@@ -2,10 +2,10 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import ReactGA from 'react-ga4';
 
-const { NODE_ENV, REACT_APP_GA_TRACKING_ID } = process.env;
+const { NODE_ENV, GA_MEASUREMENT_ID } = process.env;
 
 if (NODE_ENV === 'production') {
-  ReactGA.initialize(REACT_APP_GA_TRACKING_ID);
+  ReactGA.initialize(GA_MEASUREMENT_ID);
 }
 
 const Analytics = () => {
