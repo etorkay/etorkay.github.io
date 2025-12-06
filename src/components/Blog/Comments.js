962e3f4fbadb82512ce41482a4fa92a323d5ad65
etorkay <50207979+etorkay@users.noreply.github.com>
2025-12-06 14:35:57 +0000

    dynamic theming added to giscus box

--- src/components/Blog/Comments.js
+++ src/components/Blog/Comments.js
@@ -1,24 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Giscus from '@giscus/react';
 
-const Comments = () => (
-    <div className="comments-wrapper">
-        <Giscus
-            id="comments"
-            repo="etorkay/etorkay.github.io"
-            repoId="YOUR_REPO_ID_HERE" // User needs to provide this
-            category="Announcements"
-            categoryId="YOUR_CATEGORY_ID_HERE" // User needs to provide this
-            mapping="pathname"
-            term="Welcome to my blog!"
-            reactionsEnabled="1"
-            emitMetadata="0"
-            inputPosition="top"
-            theme="preferred_color_scheme"
-            lang="en"
-            loading="lazy"
-        />
-    </div>
-);
+const Comments = () => {
+    const [theme, setTheme] = useState('light');
+
+    useEffect(() => {
+        // Function to get current theme from document
+        const getTheme = () => document.documentElement.getAttribute('data-theme') || 'light';
+
+        // Set initial theme
+        setTheme(getTheme());
+
+        // Create observer to watch for attribute changes
+        const observer = new MutationObserver((mutations) => {
+            mutations.forEach((mutation) => {
+                if (mutation.type === 'attributes' && mutation.attributeName === 'data-theme') {
+                    setTheme(getTheme());
+                }
+            });
+        });
+
+        // Start observing
+        observer.observe(document.documentElement, {
+            attributes: true,
+            attributeFilter: ['data-theme'],
+        });
+
+        // Cleanup
+        return () => observer.disconnect();
+    }, []);
+
+    return (
+        <div className="comments-wrapper">
+            <Giscus
+                id="comments"
+                repo="etorkay/etorkay.github.io"
+                repoId="YOUR_REPO_ID_HERE"
+                category="Announcements"
+                categoryId="YOUR_CATEGORY_ID_HERE"
+                mapping="pathname"
+                term="Welcome to my blog!"
+                reactionsEnabled="1"
+                emitMetadata="0"
+                inputPosition="top"
+                theme={theme}
+                lang="en"
+                loading="lazy"
+            />
+        </div>
+    );
+};
 
 export default Comments;
